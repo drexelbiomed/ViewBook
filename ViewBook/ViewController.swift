11f033c6fd6608d1dbac5bdb9a7a30fa5548b881
David Myers <davidmyers9000@gmail.com>
2018-07-16 12:21:49 -0400

    init

---
+++ ViewBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewBook
+//
+//  Created by Dave Myers on 7/16/18.
+//  Copyright © 2018 Drexel University School of Biomedical Engineering, Science and Health Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
